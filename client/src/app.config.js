75364acdf1bf2b078ea9ccd9fd51f499c511f522
tian <tiangao0611@gmail.com>
2019-12-29 19:16:44 -0500

    a

---
+++ client/src/app.config.js
@@ -0,0 +1,6 @@
+export default {
+  url: 'https://dev-148128.okta.com',
+  issuer: 'https://dev-148128.okta.com/oauth2/default',
+  redirect_uri: window.location.origin + '/implicit/callback',
+  client_id: '0oa2cvp1ar2hDGw4K357'
+};
